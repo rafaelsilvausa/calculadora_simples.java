2b3f495ddc49737ca5c64688ff2bbe46f27badb1
rafaelsilvausa <128944830+rafaelsilvausa@users.noreply.github.com>
2023-04-21 13:24:11 -0300

    calculadora.java

---
+++ CalculadoraSimples.java/CalculadoraSimples.java/src/principal.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class principal {
+	/*
+	 * feito por Rafael do Eespirito Santo Silva
+	 */
+	
+	public static void main(String[] args) {
+		
+		int n1;
+		int n2;
+		
+		String  operacao = "";
+		int control=0;
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite a operação desejada (+, -, *, /): ");
+		operacao = entrada.nextLine();
+		
+		System.out.println("Digite um valor: ");
+		n1 = Integer.parseInt(entrada.nextLine());
+		
+		System.out.println("Digite um valor: ");
+		n2 = Integer.parseInt(entrada.next());
+		
+		if(operacao.equals("+")) {
+			System.out.println("O resultado da subtração é: " +(n1 + n2));
+			control++;			
+		}
+		
+		if(operacao.equals("-")) {
+			System.out.println("O resultado da subtração é: " +(n1 - n2));
+			control++;		
+		}
+		
+		if(operacao.equals("*")) {
+			System.out.println("O resultado da multiplicação é: " +(n1 * n2));
+			control++;			
+		}
+		
+		if(operacao.equals("/")) {
+			System.out.println("O resultado da divisão é: " +(n1 / n2));
+			control++;			
+		}
+		
+		if(control == 0) {
+			System.out.println("Oopração inválida, Tente novamente!");
+						
+		}
+		
+	}
+}
+	
+
